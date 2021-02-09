0947190513adcd9274a82e50ea13bb23485294ce
Josh Murr <joshuadavidmurr@gmail.com>
2021-02-09 15:13:59 +0000

    Arrow movement

--- src/App.js
+++ src/App.js
@@ -1,32 +1,17 @@
+import { useState } from 'react'
 import GlobalStyle from './components/GlobalStyle'
-import Slide from './components/Slide'
-import Title from './components/Title'
-import Textbox from './components/Textbox'
-import Content from './components/Content'
-import Webcam from './components/Webcam'
-import Image from './components/Image'
+import Slideshow from './components/Slideshow'
 
 import { slides_data } from './slides'
 
-function App() {
-  const slides = slides_data.map(slide => {
-    return (
-      <Slide>
-        <Title>{ slide.title }</Title>
-        <Textbox>{ slide.text }</Textbox>
-        <Content>
-          { slide.content.images.map(img => <Image src={img}></Image>) }
-        </Content>
-        <Webcam>This is the webcam.</Webcam>
-      </Slide>
-    )
-  });
 
+function App() {
+  const [slideNum, setSlideNum] = useState(0)
 
   return (
     <div className="App">
       <GlobalStyle />
-      { slides }
+			<Slideshow data={slides_data} slide={slideNum} onSlideChange={setSlideNum} />
     </div>
   );
 }
