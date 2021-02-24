95939271baf72baf8e22f4da78a30b2cb8071e7e
Josh Murr <joshuadavidmurr@gmail.com>
2021-02-24 18:32:36 +0000

    Slight format

--- src/components/Textbox.js
+++ src/components/Textbox.js
@@ -2,28 +2,33 @@ import styled from 'styled-components'
 
 const Container = styled.div`
   background-color: white;
-	grid-area: textbox;
-	padding: 20px;
-	border-radius: 15px;
-	overflow-y: scroll;
-	scrollbar-width: none;
-`;
-
-const Text = styled.p`
-	margin: 0;
-	font-size: 20px;
-	font-family: 'Fira Sans Condensed', sans-serif;
-	font-size: 1.3em;
-	line-height: 1.3em;
+  grid-area: textbox;
+  padding: 20px;
+  border-radius: 15px;
+  overflow-y: scroll;
+  scrollbar-width: none;
 `
 
+const Text = styled.div`
+  margin: 0;
+  font-size: 20px;
+  font-family: 'Fira Sans Condensed', sans-serif;
+  font-size: 1.3em;
+  line-height: 1.3em;
+  h3 {
+    margin: 0;
+    margin-bottom: 1em;
+    font-style: italic;
+    font-weight: bold;
+  }
+`
 
-function Textbox({ children }){
+function Textbox({ children }) {
   return (
     <Container>
-			<Text dangerouslySetInnerHTML={{__html: children}} />
+      <Text dangerouslySetInnerHTML={{ __html: children }} />
     </Container>
-  );
+  )
 }
 
-export default Textbox;
+export default Textbox
